f05127fd925f1dab5b6d83b98afca3b08ee96222
Benaka Moorthi <benaka.moorthi@gmail.com>
2009-09-09 23:04:12 -0400

    Added copyright header to jamfiles

--- include/charmed/metadata_storage.hpp
+++ include/charmed/metadata_storage.hpp
@@ -1,3 +1,12 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2009 Benaka Moorthi
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+///////////////////////////////////////////////////////////////////////////////
+
 #if !defined( CHARMED_DETAIL_METADATA_STORAGE_HPP )
 #define CHARMED_DETAIL_METADATA_STORAGE_HPP
 
